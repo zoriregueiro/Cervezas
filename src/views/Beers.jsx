adc41b9f67a2565196ce95d56bafc9f7af1c02fe
Zori Regueiro <zoriregueiro@gmail.com>
2022-12-06 18:04:19 +0100

    Header OK

--- src/views/Beers.jsx
+++ src/views/Beers.jsx
@@ -3,72 +3,82 @@ import { getBeers } from "../sevices";
 import { useNavigate } from "react-router-dom";
 import { useSearchParams } from "react-router-dom";
 import "./Beers.css";
-import Header from "./Header";
-
+import logo from "../imagenes/Logo.png"
+import menu from "../imagenes/Menu.png"
 
 const Beers = () => {
-  const [beers, setBeers] = useState([]);
-  const [filter, setFilter] = useState("");
-  const navigate = useNavigate();
+const [beers, setBeers] = useState([]);
+const [filter, setFilter] = useState("");
+const navigate = useNavigate();
 
-  const [searchParams, setSearchParams] = useSearchParams();
+const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    setFilter((searchParams.get("query") || "").toUpperCase());
-    const loadData = ()=> loadBeers();
-    loadData();
-     // eslint-disable-next-line
-  }, []);
- 
+useEffect(() => {
+  setFilter((searchParams.get("query") || "").toUpperCase());
+  const loadData = ()=> loadBeers();
+  loadData();
+    // eslint-disable-next-line
+}, []);
 
-  const filterData = (e) => {
-    setFilter(e.target.value.toUpperCase());
-    searchParams.set("query", e.target.value.toUpperCase());
-    setSearchParams(searchParams);
-  };
 
-  const loadBeers = async (e) => {
-    try {
-      const beers = await getBeers();
+const filterData = (e) => {
+  setFilter(e.target.value.toUpperCase());
+  searchParams.set("query", e.target.value.toUpperCase());
+  setSearchParams(searchParams);
+};
 
-      setBeers(beers);
-    } catch (error) {
-      console.error("error", e);
-    }
-  };
+const loadBeers = async (e) => {
+  try {
+    const beers = await getBeers();
 
-  function redirect(id) {
-    navigate("/beers/" + id);
+    setBeers(beers);
+  } catch (error) {
+    console.error("error", e);
   }
+};
 
-  return (
-    <div>
-      <Header></Header>
-    <div className="beer">
-      <input
-        className="toolBar"
-        placeholder="Search here"
-        type="text"
-        onChange={filterData}
-        value={filter}
-      ></input>
-      <div className="flex">
-        {beers
-          .filter((beer) => beer.filterName.includes(filter))
-          .map((beer) => (
-            <div
-              className="container"
-              onClick={() => redirect(beer.id)}
-              key={beer.id}
-            >
-              <p>{beer.name}</p> <p>{beer.tagline}</p>
-              <img alt={beer.name} src={beer.image_url} />
-            </div>
-          ))}
-      </div>
-      </div>
-      </div>
-  );
+function redirect(id) {
+  navigate("/beers/" + id);
+}
+
+return (
+  <div>
+    <header>
+<img className="logo" src={logo} alt="imagen logo" />
+<input
+  className="toolBar"
+  placeholder="Search here"
+  type="text"
+  onChange={filterData}
+  value={filter}
+  ></input>
+  <img className="menu" src={menu} alt="icono menu" />
+  </header>
+  <div className="beer">
+    <input
+      className="toolBar"
+      placeholder="Search here"
+      type="text"
+      onChange={filterData}
+      value={filter}
+    ></input>
+    <div className="flex">
+      {beers
+        .filter((beer) => beer.filterName.includes(filter))
+        .map((beer) => (
+          <div
+            className="container"
+            onClick={() => redirect(beer.id)}
+            key={beer.id}
+          >
+            <p>{beer.name}</p> <p>{beer.tagline}</p>
+            <img alt={beer.name} src={beer.image_url} />
+          </div>
+        ))}
+    </div>
+    </div>
+    </div>
+);
 };
 
 export default Beers;
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -6,31 +6,31 @@ import whatsapp from "../imagenes/whatsapp.png"
 import facebook from "../imagenes/facebook.png"
 
 function HomePage() {
-    const navigate = useNavigate(); 
-    const routeChange = () =>{ 
-      let path = "/beers"; 
-      navigate(path);
-    }
-  return <div className="home">
-      <img src={logo} alt="logo marga" />
-      <button type="button" onClick={routeChange}> Start experience </button>
-  
-    <div className="footer">
-      <div className="instagram">
-        <img src={instagram} alt="logo instagram" />
-        <p>@beer_museum</p>
-      </div>
-      <div className="whatsapp">
-        <img src={whatsapp} alt="logo whatsapp" />
-        <p> (+34) 654575865</p>
-      </div>
-      <div className="facebook">
-        <img src={facebook} alt="logo facebook" />
-        <p>@beer_museam</p>
-      </div>
+  const navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = "/beers"; 
+    navigate(path);
+  }
+return <div className="home">
+    <img src={logo} alt="logo marga" />
+    <button type="button" onClick={routeChange}> Start experience </button>
 
-  </div>
-  </div>
+  <div className="footer">
+    <div className="instagram">
+      <img src={instagram} alt="logo instagram" />
+      <p>@beer_museum</p>
+    </div>
+    <div className="whatsapp">
+      <img src={whatsapp} alt="logo whatsapp" />
+      <p> (+34) 654575865</p>
+    </div>
+    <div className="facebook">
+      <img src={facebook} alt="logo facebook" />
+      <p>@beer_museam</p>
+    </div>
+
+</div>
+</div>
 }
 
 
