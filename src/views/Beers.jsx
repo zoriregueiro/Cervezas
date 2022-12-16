64ee40a8ce23cb9e59de3c7612b1441ab641f3f5
Zori Regueiro <zoriregueiro@gmail.com>
2022-12-16 19:04:46 +0100

    ok size beers

--- src/views/Beers.jsx
+++ src/views/Beers.jsx
@@ -14,70 +14,75 @@ const navigate = useNavigate();
 const [searchParams, setSearchParams] = useSearchParams();
 
 useEffect(() => {
-  setFilter((searchParams.get("query") || "").toUpperCase());
-  const loadData = ()=> loadBeers();
-  loadData();
-    // eslint-disable-next-line
+setFilter((searchParams.get("query") || "").toUpperCase());
+const loadData = ()=> loadBeers();
+loadData();
+  // eslint-disable-next-line
 }, []);
 
 
 const filterData = (e) => {
-  setFilter(e.target.value.toUpperCase());
-  searchParams.set("query", e.target.value.toUpperCase());
-  setSearchParams(searchParams);
+setFilter(e.target.value.toUpperCase());
+searchParams.set("query", e.target.value.toUpperCase());
+setSearchParams(searchParams);
 };
 
 const loadBeers = async (e) => {
-  try {
-    const beers = await getBeers();
+try {
+  const beers = await getBeers();
 
-    setBeers(beers);
-  } catch (error) {
-    console.error("error", e);
-  }
+  setBeers(beers);
+} catch (error) {
+  console.error("error", e);
+}
 };
 
 function redirect(id) {
-  navigate("/beers/" + id);
+navigate("/beers/" + id);
 }
 
 return (
-  <div>
-    <header>
+<div>
+  <header>
 <img className="logo" src={logo} alt="imagen logo" />
 <input
-  className="toolBar"
-  placeholder="Search here"
-  type="text"
-  onChange={filterData}
-  value={filter}
-  ></input>
-  <img className="menu" src={menu} alt="icono menu" />
-  </header>
-  <div className="beer">
-      <div className="first">
-        <div className="title">
+className="toolBar"
+placeholder="Search here"
+type="text"
+onChange={filterData}
+value={filter}
+></input>
+<img className="menu" src={menu} alt="icono menu" />
+</header>
+<div className="beer">
+    <div className="first">
+      <div className="title">
         <h1>Beer Museum</h1>
         <h2>Live de beer experience in Coruña</h2>
-        </div>
+      </div>
 
-   </div>
-    <div className="flex">
-      {beers
-        .filter((beer) => beer.filterName.includes(filter))
-        .map((beer) => (
-          <div
-            className="container"
-            onClick={() => redirect(beer.id)}
-            key={beer.id}
-          >
-            <p>{beer.name}</p> <p>{beer.tagline}</p>
-            <img alt={beer.name} src={beer.image_url} />
-          </div>
-        ))}
     </div>
-    </div>
-    </div>
+<div className="flexTitle">
+  <h2>Discover our beers</h2>
+  <h3>Brewery tradition and innovation</h3>
+</div>
+<div className="flex">        
+    {beers
+      .filter((beer) => beer.filterName.includes(filter))
+      .map((beer) => (
+        <div
+          className="container"
+          onClick={() => redirect(beer.id)}
+          key={beer.id}
+        >
+          <img alt={beer.name} src={beer.image_url} />
+          <div><p>{beer.name}</p> <p>{beer.tagline}</p></div>
+          
+        </div>
+      ))}
+</div>
+</div>
+</div>
 );
 };
 
